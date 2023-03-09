077099415619422f3cb4b8b84fc0ffa141391bae
1 <yuyi.gz@163.com>
2023-03-10 00:15:11 +0800

    variables and const

---
+++ variables/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut x = 5;
+    println!("the value of x is {}", x);
+    x = 6;
+    println!("the value of x is {}", x);
+
+    const SECOND: i8 = 60;
+    println!("the value of x is {}", SECOND);
+}
